1892ae3ea51227432287239c9ad9449458be1874
Luca Marconato <2664412+LucaMarconato@users.noreply.github.com>
2022-11-30 15:33:56 +0100

    removed parsers (they are now in spatialdata)

--- src/spatialdata_io/__init__.py
+++ src/spatialdata_io/__init__.py
@@ -1,22 +1,12 @@
 from importlib.metadata import version
 
+from spatialdata_io.readers.read_metaspace import read_metaspace
+
 # from spatialdata_io import pl, pp, tl
 from spatialdata_io.readers.read_visium import read_visium
 from spatialdata_io.readers.read_xenium import read_xenium
-from spatialdata_io.readers.read_metaspace import read_metaspace
-from spatialdata_io.constructors.images_labels import image_xarray_from_numpy, labels_xarray_from_numpy
-from spatialdata_io.constructors.points import points_anndata_from_coordinates
-from spatialdata_io.constructors.circles import circles_anndata_from_coordinates
-from spatialdata_io.constructors.polygons import polygons_anndata_from_geojson
-from spatialdata_io.constructors.table import table_update_anndata
 
 __all__ = [
-    "image_xarray_from_numpy",
-    "labels_xarray_from_numpy",
-    "points_anndata_from_coordinates",
-    "circles_anndata_from_coordinates",
-    "polygons_anndata_from_geojson",
-    "table_update_anndata",
     "read_metaspace",
     "read_visium",
     "read_xenium",
