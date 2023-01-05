b93e109b8db5202206cb6e3a7ccb0dbd761ca44d
giovp <giov.pll@gmail.com>
2023-01-05 18:20:42 +0100

    fix init and pre-commit

--- src/spatialdata_io/__init__.py
+++ src/spatialdata_io/__init__.py
@@ -1,15 +1,15 @@
 from importlib.metadata import version
 
-from spatialdata_io.readers.metaspace import read_metaspace
+from spatialdata_io.readers.cosmx import cosmx
 
 # from spatialdata_io import pl, pp, tl
 from spatialdata_io.readers.visium import read_visium
 from spatialdata_io.readers.xenium import xenium
 
 __all__ = [
-    "read_metaspace",
     "read_visium",
-    "convert_xenium_to_ngff",
+    "xenium",
+    "cosmx",
 ]
 
 __version__ = version("spatialdata-io")
