5ec1ba1ebecc0fcefe10cdb22fe7c49aba62e557
LucaMarconato <2664412+LucaMarconato@users.noreply.github.com>
2023-03-08 19:34:07 +0100

    added a TODO

--- src/spatialdata_io/readers/visium.py
+++ src/spatialdata_io/readers/visium.py
@@ -145,6 +145,10 @@ def visium(
         transformations={"global": transform_original},
         **image_models_kwargs,
     )
+    # TODO: here we are returning three different coordiante systems for the same data. We are considering
+    # simplifying the way xarray coordinates and NGFF coordinate transformations interact. After this we 
+    # will most likely return one object will only one coordinate system and all the coordinates aligned
+    # More details can be found here: https://github.com/scverse/spatialdata/issues/169#issuecomment-1458022514
     image_hires_parsed = Image2DModel.parse(image_hires, transformations={"downscaled_hires": Identity()})
     image_lowres_parsed = Image2DModel.parse(image_lowres, transformations={"downscaled_lowres": Identity()})
 
