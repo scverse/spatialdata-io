6e3b908b7d37d4ee0f3a6904c4b23014aa554fc4
Laurens Lehner <laurenslehner@googlemail.com>
2023-05-22 11:45:46 +0200

    Update

--- src/spatialdata_io/readers/codex.py
+++ src/spatialdata_io/readers/codex.py
@@ -5,15 +5,16 @@ import re
 from collections.abc import Mapping
 from pathlib import Path
 from types import MappingProxyType
-from typing import Any
+from typing import Any, Optional
 
-import anndata as ad
-import imageio.v3 as iio
 import pandas as pd
+import anndata as ad
 import readfcs
+import imageio.v3 as iio
 from spatialdata import SpatialData
 from spatialdata._logging import logger
-from spatialdata.models import Image2DModel, TableModel
+from spatialdata.models import TableModel
+from spatialdata.models import Image2DModel
 
 from spatialdata_io._constants._constants import CodexKeys
 from spatialdata_io._docs import inject_docs
@@ -46,7 +47,7 @@ def codex(
     path
         Path to the directory containing the data.
     fcs
-        Whether a ``.fcs`` is provided. If ``False``, a ``.csv`` file is expected.
+        Whether a .fcs file is provided. If False, a .csv file is expected.
     imread_kwargs
         Keyword arguments passed to :func:`dask_image.imread.imread`.
     image_models_kwargs
@@ -56,49 +57,42 @@ def codex(
     -------
     :class:`spatialdata.SpatialData`
     """
+
     path = Path(path)
-    patt = re.compile(f"{CodexKeys.FCS_FILE}") if fcs else re.compile(f"{CodexKeys.FCS_FILE_CSV}")
+    patt = re.compile(f".*.fcs") if fcs else re.compile(f".*.csv") 
     path_files = [i for i in os.listdir(path) if patt.match(i)]
-    if path_files:
-        fcs = (
-            readfcs.ReadFCS(path_files[0]).data
-            if f"{CodexKeys.FCS_FILE}" in patt.pattern
-            else pd.read_csv(path_files[0], header=0, index_col=None)
+    if path_files and ".fcs" or ".csv" in patt.pattern:
+        fcs = readfcs.ReadFCS(path / path_files[0]).data if ".fcs" in path_files[0] else pd.read_csv(path_files[0], header=0, index_col=None)
+    else:
+        raise ValueError(
+            f"Cannot determine data set. Expecting a file with format .fcs or .csv"
         )
-    else:
-        raise ValueError("Cannot determine data set. Expecting a file with format .fcs or .csv")
 
-    adata = _df_to_anndata(fcs)
+    adata = _codex_df_to_anndata(fcs)
 
-    region = adata.obs[f"{CodexKeys.REGION_KEY}"].unique()[0].tolist()
-    table = TableModel.parse(
-        adata, region=region, region_key=f"{CodexKeys.REGION_KEY}", instance_key=f"{CodexKeys.INSTANCE_KEY}"
-    )
+    region = adata.obs["region"].unique()[0].tolist()
+    table = TableModel.parse(adata, region=region, region_key="region", instance_key="cell_id")
 
-    im_patt = re.compile(f"{CodexKeys.IMAGE_TIF}")
+    im_patt = re.compile(f".*.tif") 
     path_files = [i for i in os.listdir(path) if im_patt.match(i)]
-    if path_files and f"{CodexKeys.IMAGE_TIF}" in path_files[0]:
-        image = iio.imread(path_files[0], **imread_kwargs)
-        images = {
-            "images": Image2DModel.parse(
-                image,
-                scale_factors=[2, 2, 2, 2],
-                **image_models_kwargs,
-            )
-        }
-        sdata = SpatialData(image=images, table=table)
+    if path_files and  ".tif" in path_files[0]:
+        image = iio.imread(path_files[0])
+        images = {"images": Image2DModel.parse(image,scale_factors=[2, 2],)}
+        sdata = SpatialData(images=images, table=table)
     else:
-        logger.warning(f"Cannot find {CodexKeys.IMAGE_TIF} file. Will build spatialdata with table only.")
+        logger.warning(
+                f"Cannot find .tif file. Will build spatialdata with table only."
+            )
         sdata = SpatialData(table=table)
 
     return sdata
 
-
-def _df_to_anndata(df: pd.DataFrame) -> ad.AnnData:
-    """Convert a DataFrame to an anndata object."""
+def _codex_df_to_anndata(df: pd.DataFrame) -> ad.AnnData:
+    """
+    Convert a dataframe made from a codex formatted .fcs or .csv file to anndata.
+    """
     adata = ad.AnnData(df.filter(regex="cyc.*"))
-    adata.obs = df[df.columns.drop(list(df.filter(regex="cyc.*")))]
-    adata.obs.set_index("cell_id", inplace=True, drop=False)
-    adata.obsm["spatial"] = df[["x", "y"]].values
+    adata.obs = df[df.columns.drop(list(df.filter(regex='cyc.*')))]
+    adata.obsm["spatial"] = df[['x', 'y']].values
     adata.var_names_make_unique()
     return adata
