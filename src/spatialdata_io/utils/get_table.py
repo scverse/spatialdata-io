4eb21957385eabb8a3590c13d4e01519d6af052e
LLehner <laurenslehner@googlemail.com>
2023-06-06 16:02:22 +0200

    Add function to retrieve table from spatialdata object

---
+++ src/spatialdata_io/utils/get_table.py
@@ -0,0 +1,9 @@
+from anndata import AnnData
+from spatialdata import SpatialData
+
+__all__ = ["get_table"]
+
+
+def get_table(sdata: SpatialData) -> AnnData:
+    """Retrieve table from SpatialData."""
+    return sdata.table.copy()
