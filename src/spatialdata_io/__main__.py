3dfcee97016d30250ff45956a17a66c85ceb66c2
Miguel A. Ibarra-Arellano <ibarrarellano@gmail.com>
2024-11-13 19:12:13 +0100

    Add click parser for all available readers.
    Co-authored-by: Chiara Schiller <chiara_schiller@t-online.de>
    
    Co-authored-by: Florian Wuennemann <flowuenne@gmail.com>

--- src/spatialdata_io/__main__.py
+++ src/spatialdata_io/__main__.py
@@ -4,108 +4,322 @@ import click
 import os
 from pathlib import Path
 from pkg_resources import require
+from spatialdata_io._constants._constants import VisiumKeys
+from spatialdata_io import __readers__
+from spatialdata_io.readers.codex import codex
+from spatialdata_io.readers.cosmx import cosmx
+from spatialdata_io.readers.curio import curio
+from spatialdata_io.readers.dbit import dbit
+from spatialdata_io.readers.iss import iss
+from spatialdata_io.readers.mcmicro import mcmicro
+from spatialdata_io.readers.merscope import merscope
+from spatialdata_io.readers.seqfish import seqfish
+from spatialdata_io.readers.steinbock import steinbock
+from spatialdata_io.readers.stereoseq import stereoseq
+from spatialdata_io.readers.visium import visium
+from spatialdata_io.readers.visium_hd import visium_hd
+from spatialdata_io.readers.xenium import xenium
 
 
-def codex_wrapper(input, output):
-    from spatialdata_io.readers.codex import codex
-    sdata = codex(input)
+@click.group()
+def cli():
+    """
+    Convert standard technology data formats to SpatialData object.
+
+    Usage:
+
+    python -m spatialdata-io <Command> -i <input> -o <output>
+
+    For help on how to use a specific command, run:
+
+    python -m spatialdata-io <Command> --help
+    """
+    pass
+
+
+@cli.command(name="codex")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--fcs', type=bool, default=True, help='FCS')
+def codex_wrapper(input,
+                  output,
+                  fcs=True):
+    """Codex conversion to SpatialData"""
+    sdata = codex(input, fcs)
     sdata.write(output)
 
-def cosmx_wrapper(input, output):
-    from spatialdata_io.readers.cosmx import cosmx
-    sdata = cosmx(input)
+
+@cli.command(name="cosmx")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--dataset_id', type=str, default=None, help='Dataset ID')
+@click.option('--transcripts', type=bool, default=True, help='Transcripts')
+def cosmx_wrapper(input,
+                  output,
+                  dataset_id=None,
+                  transcripts=True):
+    """Cosmic conversion to SpatialData"""
+    sdata = cosmx(input,
+                  dataset_id,
+                  transcripts)
     sdata.write(output)
 
-def curio_wrapper(input, output):
-    from spatialdata_io.readers.curio import curio
+
+@cli.command(name="curio")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+def curio_wrapper(input,
+                  output):
+    """Curio conversion to SpatialData"""
     sdata = curio(input)
     sdata.write(output)
 
-def dbit_wrapper(input, output):
-    from spatialdata_io.readers.dbit import dbit
-    sdata = dbit(input)
+
+@cli.command(name="dbit")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--pattern', type=str, default=None, help='Pattern')
+@click.option('--key', type=str, default=None, help='Key')
+@click.option('--path_specific', type=bool, default=None, help='Path specific')
+@click.option('--optional_arg', type=bool, default=False, help='Optional argument')
+def dbit_wrapper(input,
+                 output,
+                 pattern,
+                 key,
+                 path_specific=None,
+                 optional_arg=False):
+    """DBiT conversion to SpatialData"""
+    sdata = dbit(input,
+                 pattern,
+                 key,
+                 path_specific,
+                 optional_arg)
     sdata.write(output)
 
-def iss_wrapper(input, output):
-    from spatialdata_io.readers.iss import iss
-    sdata = iss(input)
+
+@cli.command(name="iss")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--instance_key', type=str, default=None, help='Instance key')
+@click.option('--dataset_id', type=str, default='region', help='Dataset ID')
+@click.option('--multiscale_image', type=bool, default=True, help='Multiscale image')
+@click.option('--multiscale_labels', type=bool, default=True, help='Multiscale labels')
+def iss_wrapper(input,
+                output,
+                raw_relative_path,
+                labels_relative_path,
+                h5ad_relative_path,
+                instance_key=None,
+                dataset_id="region",
+                multiscale_image=True,
+                multiscale_labels=True):
+    """ISS conversion to SpatialData"""
+    sdata = iss(input,
+                raw_relative_path,
+                labels_relative_path,
+                h5ad_relative_path,
+                instance_key=instance_key,
+                dataset_id=dataset_id,
+                multiscale_image=multiscale_image,
+                multiscale_labels=multiscale_labels)
     sdata.write(output)
 
+
+@cli.command(name="mcmicro")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
 def mcmicro_wrapper(input, output):
-    from spatialdata_io.readers.mcmicro import mcmicro
+    """MCMicro conversion to SpatialData"""
     sdata = mcmicro(input)
     sdata.write(output)
 
-def merscope_wrapper(input, output):
-    from spatialdata_io.readers.merscope import merscope
-    sdata = merscope(input)
+
+@cli.command(name="merscope")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--vpt_output', type=str, default=None, help='VPT output')
+@click.option('--z_layers', type=int, default=3, help='Z layers')
+@click.option('--region_name', type=str, default=None, help='Region name')
+@click.option('--slide_name', type=str, default=None, help='Slide name')
+@click.option('--backend', type=str, default=None, help='Backend')
+@click.option('--transcripts', type=bool, default=True, help='Transcripts')
+@click.option('--cells_boundaries', type=bool, default=True, help='Cells boundaries')
+@click.option('--cells_table', type=bool, default=True, help='Cells table')
+@click.option('--mosaic_images', type=bool, default=True, help='Mosaic images')
+def merscope_wrapper(input,
+                     output,
+                     vpt_output=None,
+                     z_layers=3,
+                     region_name=None,
+                     slide_name=None,
+                     backend=None,
+                     transcripts=True,
+                     cells_boundaries=True,
+                     cells_table=True,
+                     mosaic_images=True):
+    """Merscope conversion to SpatialData"""
+    sdata = merscope(input,
+                     vpt_output=vpt_output,
+                     z_layers=z_layers,
+                     region_name=region_name,
+                     slide_name=slide_name,
+                     backend=backend,
+                     transcripts=transcripts,
+                     cells_boundaries=cells_boundaries,
+                     cells_table=cells_table,
+                     mosaic_images=mosaic_images)
     sdata.write(output)
 
-def seqfish_wrapper(input, output):
-    from spatialdata_io.readers.seqfish import seqfish
-    sdata = seqfish(input)
+
+@cli.command(name="seqfish")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--load_images', type=bool, default=True, help='Load images')
+@click.option('--load_labels', type=bool, default=True, help='Load labels')
+@click.option('--load_points', type=bool, default=True, help='Load points')
+@click.option('--sections', type=int, default=None, help='Sections')
+def seqfish_wrapper(input,
+                    output,
+                    load_images=True,
+                    load_labels=True,
+                    load_points=True,
+                    sections=None):
+    """Seqfish conversion to SpatialData"""
+    sdata = seqfish(input,
+                    load_images=load_images,
+                    load_labels=load_labels,
+                    load_points=load_points,
+                    sections=sections)
     sdata.write(output)
 
-def steinbock_wrapper(input, output):
-    from spatialdata_io.readers.steinbock import steinbock
-    sdata = steinbock(input)
+
+@cli.command(name="steinbock")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--labels_kind', type=str, default='deepcell', help='Labels kind')
+def steinbock_wrapper(input,
+                      output,
+                      labels_kind="deepcell"):
+    """Steinbock conversion to SpatialData"""
+    sdata = steinbock(input,
+                      labels_kind=labels_kind)
     sdata.write(output)
 
-def stereoseq_wrapper(input, output):
-    from spatialdata_io.readers.stereoseq import stereoseq
-    sdata = stereoseq(input)
+
+@cli.command(name="stereoseq")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--dataset_id', type=str, default=None, help='Dataset ID')
+@click.option('--read_square_bin', type=bool, default=True, help='Read square bin')
+@click.option('--optional_tif', type=bool, default=False, help='Optional TIF')
+def stereoseq_wrapper(input,
+                      output,
+                      dataset_id=None,
+                      read_square_bin=True,
+                      optional_tif=False):
+    """Stereoseq conversion to SpatialData"""
+    sdata = stereoseq(input,
+                      dataset_id=dataset_id,
+                      read_square_bin=read_square_bin,
+                      optional_tif=optional_tif)
     sdata.write(output)
 
-def visium_wrapper(input, output):
-    from spatialdata_io.readers.visium import visium
-    sdata = visium(input)
+
+@cli.command(name="visium")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--dataset_id', type=str, default=None, help='Dataset ID')
+@click.option('--counts_file', type=str, default=VisiumKeys.FILTERED_COUNTS_FILE, help='Counts file')
+@click.option('--fullres_image_file', type=str, default=None, help='Fullres image file')
+@click.option('--tissue_positions_file', type=str, default=None, help='Tissue positions file')
+@click.option('--scalefactors_file', type=str, default=None, help='Scalefactors file')
+def visium_wrapper(input,
+                   output,
+                   dataset_id=None,
+                   counts_file=VisiumKeys.FILTERED_COUNTS_FILE,
+                   fullres_image_file=None,
+                   tissue_positions_file=None,
+                   scalefactors_file=None):
+    """Visium conversion to SpatialData"""
+    sdata = visium(input,
+                   dataset_id=dataset_id,
+                   counts_file=counts_file,
+                   fullres_image_file=fullres_image_file,
+                   tissue_positions_file=tissue_positions_file,
+                   scalefactors_file=scalefactors_file)
     sdata.write(output)
 
-def visium_hd_wrapper(input, output):
-    from spatialdata_io.readers.visium_hd import visium_hd
-    sdata = visium_hd(input)
+
+@cli.command(name="visium_hd")
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--dataset_id', type=str, default=None, help='Dataset ID')
+@click.option('--filtercounts_file', type=bool, default=True, help='Filtercounts file')
+@click.option('--bin_size', type=int, default=None, help='Bin size')
+@click.option('--bins_s_squares', type=bool, default=True, help='Bins s squares')
+@click.option('--fullres_image_file', type=str, default=None, help='Fullres image file')
+@click.option('--load_all_images', type=bool, default=False, help='Load all images')
+def visium_hd_wrapper(input,
+                      output,
+                      dataset_id=None,
+                      filtercounts_file=True,
+                      bin_size=None,
+                      bins_s_squares=True,
+                      fullres_image_file=None,
+                      load_all_images=False):
+    """Visium HD conversion to SpatialData"""
+    sdata = visium_hd(input,
+                      dataset_id=dataset_id,
+                      filtercounts_file=filtercounts_file,
+                      bin_size=bin_size,
+                      bins_s_squares=bins_s_squares,
+                      fullres_image_file=fullres_image_file,
+                      load_all_images=load_all_images)
     sdata.write(output)
 
-def xenium_wrapper(input, output):
-    from spatialdata_io.readers.xenium import xenium
-    sdata = xenium(input)
-    sdata.write(output)
 
-@click.command()
-@click.option('--input', '-i', type=click.Path(exists=True), help='Input file path')
-@click.option('--output', '-o', type=click.Path(), help='Output file path')
-@click.option('--reader_name', '-r', type=click.Choice(["codex", "cosmx", "curio", "dbit", "iss", "mcmicro", "merscope", "seqfish", "steinbock", "stereoseq", "visium", "visium_hd", "xenium"]), help='Reader name to use for conversion')
-def main(input, output, reader_name):
-    # For Codex
-    if reader_name == 'codex':
-        codex_wrapper(input, output)
-    elif reader_name == 'cosmx':
-        cosmx_wrapper(input, output)
-    elif reader_name == 'curio':
-        curio_wrapper(input, output)
-    elif reader_name == 'dbit':
-        dbit_wrapper(input, output)
-    elif reader_name == 'iss':
-        iss_wrapper(input, output)
-    elif reader_name == 'mcmicro':
-        mcmicro_wrapper(input, output)
-    elif reader_name == 'merscope':
-        merscope_wrapper(input, output)
-    elif reader_name == 'seqfish':
-        seqfish_wrapper(input, output)
-    elif reader_name == 'steinbock':
-        steinbock_wrapper(input, output)
-    elif reader_name == 'stereoseq':
-        stereoseq_wrapper(input, output)
-    elif reader_name == 'visium':
-        visium_wrapper(input, output)
-    elif reader_name == 'visium_hd':
-        visium_hd_wrapper(input, output)
-    elif reader_name == 'xenium':
-        xenium_wrapper(input, output)
-    else:
-        raise ValueError(f'Invalid reader name, choose from {["codex", "cosmx", "curio", "dbit", "iss", "mcmicro", "merscope", "seqfish", "steinbock", "stereoseq", "visium", "visum_hd", "xenium"]}')
+@cli.command(name='xenium')
+@click.option('--input', '-i', type=click.Path(exists=True), help="Path to the input file.", required=True)
+@click.option('--output', '-o', type=click.Path(), help="Path to the input file.", required=True)
+@click.option('--cells_boundaries', type=bool, default=True, help='Cells boundaries')
+@click.option('--nucleus_boundaries', type=bool, default=True, help='Nucleus boundaries')
+@click.option('--cells_as_circles', type=bool, default=None, help='Cells as circles')
+@click.option('--cells_labels', type=bool, default=True, help='Cells labels')
+@click.option('--nucleus_labels', type=bool, default=True, help='Nucleus labels')
+@click.option('--transcripts', type=bool, default=True, help='Transcripts')
+@click.option('--morphology_mip', type=bool, default=True, help='Morphology MIP')
+@click.option('--morphology_focus', type=bool, default=True, help='Morphology focus')
+@click.option('--aligned_images', type=bool, default=True, help='Aligned images')
+@click.option('--cells_table', type=bool, default=True, help='Cells table')
+@click.option('--n_jobs', type=int, default=1, help='Number of jobs')
+def xenium_wrapper(input,
+                   output,
+                   cells_boundaries=True,
+                   nucleus_boundaries=True,
+                   cells_as_circles=None,
+                   cells_labels=True,
+                   nucleus_labels=True,
+                   transcripts=True,
+                   morphology_mip=True,
+                   morphology_focus=True,
+                   aligned_images=True,
+                   cells_table=True,
+                   n_jobs=1):
+    """Xenium conversion to SpatialData"""
+    sdata = xenium(input,
+                   cells_boundaries=cells_boundaries,
+                   nucleus_boundaries=nucleus_boundaries,
+                   cells_as_circles=cells_as_circles,
+                   cells_labels=cells_labels,
+                   nucleus_labels=nucleus_labels,
+                   transcripts=transcripts,
+                   morphology_mip=morphology_mip,
+                   morphology_focus=morphology_focus,
+                   aligned_images=aligned_images,
+                   cells_table=cells_table,
+                   n_jobs=n_jobs)
+    sdata.write(output)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
