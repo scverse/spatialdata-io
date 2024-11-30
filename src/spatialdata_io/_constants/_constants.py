d69b75060cf70c0d1a2c710a31280e02d7428f31
Pratik Mehta <71743538+Pancreas-Pratik@users.noreply.github.com>
2024-11-29 20:33:04 -0500

    Update _constants.py - add spec needed for correct xenium ranger version detection after resegment
    needed within xenium.py for correct xenium() parsing/branching of /outs/ folder path input generated from the xeniumranger v3.0.1.1 resegment

--- src/spatialdata_io/_constants/_constants.py
+++ src/spatialdata_io/_constants/_constants.py
@@ -145,6 +145,8 @@ class XeniumKeys(ModeEnum):
 
     # specs keys
     ANALYSIS_SW_VERSION = "analysis_sw_version"
+    XENIUM_RANGER = "xenium_ranger" #spec which contains xeniumranger version (after resegment is run from xenium-3.0.1.1)
+
 
     # zarr file with labels file and cell summary keys
     CELLS_ZARR = "cells.zarr.zip"
