2e228cf42fe34c8831edc23132ded3825e6a8007
Luca Marconato <m.lucalmer@gmail.com>
2026-01-08 16:10:21 +0100

    wip support for polygons with 3 vertices without filtering

--- src/spatialdata_io/readers/xenium.py
+++ src/spatialdata_io/readers/xenium.py
@@ -259,33 +259,21 @@ def xenium(
                     table.uns[TableModel.ATTRS_KEY][TableModel.INSTANCE_KEY] = "cell_labels"
 
     if nucleus_boundaries:
-        invalid_nuc_ids = _find_invalid_ids(path, XeniumKeys.NUCLEUS_BOUNDARIES_FILE)
-        if len(invalid_nuc_ids) > 0:
-            logging.warning(
-                f"Found {len(invalid_nuc_ids)} invalid polygons for nuclei, removing the masks corresponding to the IDs: {invalid_nuc_ids}"
-            )
         polygons["nucleus_boundaries"] = _get_polygons(
             path,
             XeniumKeys.NUCLEUS_BOUNDARIES_FILE,
             specs,
             n_jobs,
             idx=table.obs[str(XeniumKeys.CELL_ID)].copy(),
-            invalid_ids=invalid_nuc_ids,
         )
 
     if cells_boundaries:
-        invalid_cell_ids = _find_invalid_ids(path, XeniumKeys.CELL_BOUNDARIES_FILE)
-        if len(invalid_cell_ids) > 0:
-            logging.warning(
-                f"Found {len(invalid_cell_ids)} invalid polygons for cells, removing the masks corresponding to the IDs: {invalid_cell_ids}"
-            )
         polygons["cell_boundaries"] = _get_polygons(
             path,
             XeniumKeys.CELL_BOUNDARIES_FILE,
             specs,
             n_jobs,
             idx=table.obs[str(XeniumKeys.CELL_ID)].copy(),
-            invalid_ids=invalid_cell_ids,
         )
 
     if transcripts:
@@ -394,10 +382,11 @@ def xenium(
             del image_models_kwargs["c_coords"]
             logger.removeFilter(IgnoreSpecificMessage())
 
-    if table is not None:
-        valid_nucleus_mask = ~table.obs[XeniumKeys.CELL_ID].isin(invalid_nuc_ids)
-        valid_cell_mask = ~table.obs[XeniumKeys.CELL_ID].isin(invalid_cell_ids)
-        tables["table"] = table[valid_nucleus_mask & valid_cell_mask].copy()
+    # if table is not None:
+    #     valid_nucleus_mask = ~table.obs[XeniumKeys.CELL_ID].isin(invalid_nuc_ids)
+    #     valid_cell_mask = ~table.obs[XeniumKeys.CELL_ID].isin(invalid_cell_ids)
+    #     tables["table"] = table[valid_nucleus_mask & valid_cell_mask].copy()
+    tables["table"] = table
 
     elements_dict = {
         "images": images,
@@ -419,16 +408,6 @@ def xenium(
     return sdata
 
 
-def _find_invalid_ids(
-    path: Path,
-    file: str,
-) -> ArrayLike:
-    """Filter out cell ids with too few vertices to form a valid polygon."""
-    df = pq.read_table(path / file).to_pandas()
-    invalid_ids = df.groupby(XeniumKeys.CELL_ID).filter(lambda x: len(x) < 4)[XeniumKeys.CELL_ID].unique()
-    return [] if len(invalid_ids) == 0 else invalid_ids
-
-
 def _decode_cell_id_column(cell_id_column: pd.Series) -> pd.Series:
     if isinstance(cell_id_column.iloc[0], bytes):
         return cell_id_column.apply(lambda x: x.decode("utf-8"))
@@ -441,27 +420,30 @@ def _get_polygons(
     specs: dict[str, Any],
     n_jobs: int,
     idx: ArrayLike | None = None,
-    invalid_ids: ArrayLike | None = None,
 ) -> GeoDataFrame:
-    def _poly(arr: ArrayLike) -> Polygon:
-        return Polygon(arr[:-1])
-
-    if invalid_ids is None:
-        invalid_ids = []
-
     # seems to be faster than pd.read_parquet
     df = pq.read_table(path / file).to_pandas()
     df[XeniumKeys.CELL_ID] = _decode_cell_id_column(df[XeniumKeys.CELL_ID])
 
-    # Filter based on valid cell IDs if idx is provided
-    if idx is not None:
-        idx = idx[~idx.isin(invalid_ids)]
-        if len(invalid_ids) > 0:
-            idx = idx.reset_index(drop=True)
-        df = df[df[XeniumKeys.CELL_ID].isin(idx)]
-    else:
-        # If no idx provided, just (potentially) filter out invalid IDs
-        df = df[~df[XeniumKeys.CELL_ID].isin(invalid_ids)]
+    # # filter out cell ids with too few vertices to form a valid polygon.
+    # invalid_ids = df.groupby(XeniumKeys.CELL_ID).filter(lambda x: len(x) < 3)[
+    #     XeniumKeys.CELL_ID].unique()
+    # invalid_ids = [] if len(invalid_ids) == 0 else invalid_ids
+    #
+    # if len(invalid_ids) > 0:
+    #     logging.warning(
+    #         f"Found {len(invalid_ids)} invalid polygons for {file}, removing the masks corresponding to the IDs: {invalid_ids}"
+    #     )
+    #
+    # # Filter based on valid cell IDs if idx is provided
+    # if idx is not None:
+    #     idx = idx[~idx.isin(invalid_ids)]
+    #     if len(invalid_ids) > 0:
+    #         idx = idx.reset_index(drop=True)
+    #     df = df[df[XeniumKeys.CELL_ID].isin(idx)]
+    # else:
+    #     # If no idx provided, just (potentially) filter out invalid IDs
+    #     df = df[~df[XeniumKeys.CELL_ID].isin(invalid_ids)]
 
     group_by = df.groupby(XeniumKeys.CELL_ID)
     index = pd.Series(group_by.indices.keys())
@@ -470,7 +452,7 @@ def _get_polygons(
     index = _decode_cell_id_column(index)
 
     out = Parallel(n_jobs=n_jobs)(
-        delayed(_poly)(i.to_numpy())
+        delayed(Polygon)(i.to_numpy())
         for _, i in group_by[[XeniumKeys.BOUNDARIES_VERTEX_X, XeniumKeys.BOUNDARIES_VERTEX_Y]]
     )
     geo_df = GeoDataFrame({"geometry": out})
